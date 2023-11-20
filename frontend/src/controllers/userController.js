3b008547cf511646ec07fe97192741fc8faaf4fb
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-20 17:52:10 -0500

    removed code in userController

--- frontend/src/controllers/userController.js
+++ frontend/src/controllers/userController.js
@@ -1,18 +1,1 @@
-// frontend/src/controllers/UserController.js
-import axios from 'axios';
 
-const baseURL = "http://localhost:8000";
-
-const UserController = {
-    login: async (email, password) => {
-        const response = await axios.post(`${baseURL}/api/user/login`, { email, password });
-        return response.data;
-    },
-    logout: () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-    }
-    // Other user-related methods can be added here
-};
-
-export default UserController;
